9db9e1f945b47e6e8cc5cb12be119a0567ef1a86
haruto17 <s1902272@sendai-nct.jp>
2022-06-19 16:19:05 +0900

    修正

--- docs/util.ts
+++
@@ -1,7 +0,0 @@
-const scroll_to_top_btn = document.querySelector("#scroll-to-top-button");
-
-scroll_to_top_btn?.addEventListener("click", scroll_to_top);
-
-function scroll_to_top() {
-	window.scroll({ top: 0, behavior: "smooth" });
-}
